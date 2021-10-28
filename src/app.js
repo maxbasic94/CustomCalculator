a2668a7bb74f29b53bd3e521a0a5606a2bee7cd1
Maksim Ashmiana <max.basic.94@gmai.com>
2021-10-28 12:02:44 +0300

    refactored code

--- src/app.js
+++ src/app.js
@@ -28,6 +28,98 @@ function getNumberInRoot(power) {
     }
 }
 
+function performNumByPower() {
+    if (!document.querySelector(".result").value) {return};
+    let actualNumber = document.querySelector(".result").value;
+    document.querySelector('.archiv').value = actualNumber + '^';
+    document.querySelector('.result').value = '';
+    document.querySelector('.result').placeholder = '';
+}
+
+function performPercentOperation() {
+    if (!document.querySelector(".result").value) {return};
+    let actualNumber = document.querySelector(".result").value;
+    document.querySelector('.archiv').value = actualNumber + '%';
+    document.querySelector(".result").value /= 100;
+}
+
+function performNaturalLog() {
+    if (!document.querySelector(".result").value) {return};
+    let actualNumber = document.querySelector(".result").value;
+    document.querySelector(".archiv").value = 'ln(' + actualNumber + ')';
+    document.querySelector('.result').value = Math.log(actualNumber);
+}
+
+function performDecimalLog() {
+    if (!document.querySelector(".result").value) {return};
+    let actualNumber = document.querySelector(".result").value;
+    document.querySelector(".archiv").value = 'log10(' + actualNumber + ')';
+    document.querySelector('.result').value = Math.log10(actualNumber);
+}
+
+function performExpByPower() {
+    if (!document.querySelector(".result").value) {return};
+    let actualNumber = document.querySelector(".result").value;
+    document.querySelector(".archiv").value = 'exp(' + actualNumber + ')';
+    document.querySelector('.result').value = Math.exp(actualNumber);
+}
+
+function performNumRoot() {
+    if (!document.querySelector(".result").value) {return};
+    let actualNumber = document.querySelector(".result").value;
+    document.querySelector('.archiv').value = actualNumber + '\u221A';
+    document.querySelector('.result').value = '';
+    document.querySelector('.result').placeholder = '';
+}
+
+function performClearOperation() {
+    document.querySelector('.result').value = '';
+    document.querySelector('.archiv').value = '';
+    document.querySelector('.result').placeholder = '0';
+}
+
+function performBackspaceOperation() {
+    let numberWithoutLastSymbol = document.querySelector(".result").value.slice(0, -1);
+    document.querySelector(".result").value = numberWithoutLastSymbol;
+}
+
+function performEqualOperation() {
+    let res;
+    if (!document.querySelector('.archiv').value) {return};
+    let sign = document.querySelector('.archiv').value.slice(-1);
+    let firstNumber = Number(document.querySelector('.archiv').value.slice(0, -1));
+    let secondNumber = Number(document.querySelector('.result').value);
+    switch (sign) {
+        case '+':
+            res = firstNumber + secondNumber;
+          break;
+        case '-':
+            res = firstNumber - secondNumber;
+          break;
+        case '*':
+            res = firstNumber * secondNumber;
+          break;
+        case '/':
+            if (secondNumber === 0) {
+                document.querySelector('.result').value = 'division by zero';
+                return
+            } else {
+                res = firstNumber / secondNumber;
+            }
+          break;
+        case '^':
+                res = Math.pow(firstNumber, secondNumber);
+          break;
+        case '√':
+                res = Math.pow(firstNumber, 1 / secondNumber);
+          break;
+        default:
+            return;
+    }
+    document.querySelector('.archiv').value += secondNumber;
+    document.querySelector('.result').value = res;
+}
+
 function performMemoryOptions(memoryButton) {
     switch (memoryButton) {
         case 'memoryClearButton':
@@ -76,7 +168,6 @@ const logButton = document.querySelector('#logButton');
 const memoryButtons = document.querySelectorAll('.memory');
 const numByPowerButton = document.querySelector('#numByPowerButton');
 
-
 for (let numberButton of numberButtons) {
     numberButton.addEventListener('click', function(event) {
         let number = event.target.value;
@@ -89,124 +180,22 @@ for (let memoryButton of memoryButtons) {
     memoryButton.addEventListener('click', (e) => performMemoryOptions(e.target.id));
 }
 
-lnButton.addEventListener('click', () => {
-    if (!document.querySelector(".result").value) {return};
-    let actualNumber = document.querySelector(".result").value;
-    document.querySelector(".archiv").value = 'ln(' + actualNumber + ')';
-    document.querySelector('.result').value = Math.log(actualNumber);
-})
-
-logButton.addEventListener('click', () => {
-    if (!document.querySelector(".result").value) {return};
-    let actualNumber = document.querySelector(".result").value;
-    document.querySelector(".archiv").value = 'log10(' + actualNumber + ')';
-    document.querySelector('.result').value = Math.log10(actualNumber);
-})
-
-expByPowerButton.addEventListener('click', () => {
-    if (!document.querySelector(".result").value) {return};
-    let actualNumber = document.querySelector(".result").value;
-    document.querySelector(".archiv").value = 'exp(' + actualNumber + ')';
-    document.querySelector('.result').value = Math.exp(actualNumber);
-})
-
+lnButton.addEventListener('click', performNaturalLog);
+logButton.addEventListener('click', performDecimalLog);
+expByPowerButton.addEventListener('click', performExpByPower);
 squareRootButton.addEventListener('click', () => getNumberInRoot(2));
-
 cubeRootButton.addEventListener('click', () => getNumberInRoot(3))
-
-numRootButton.addEventListener('click', () => {
-    if (!document.querySelector(".result").value) {return};
-    let actualNumber = document.querySelector(".result").value;
-    document.querySelector('.archiv').value = actualNumber + '\u221A';
-    document.querySelector('.result').value = '';
-    document.querySelector('.result').placeholder = '';
-})
-
-numByPowerButton.addEventListener('click', () => {
-    if (!document.querySelector(".result").value) {return};
-    let actualNumber = document.querySelector(".result").value;
-    document.querySelector('.archiv').value = actualNumber + '^';
-    document.querySelector('.result').value = '';
-    document.querySelector('.result').placeholder = '';
-});
-
-percentButton.addEventListener('click', () => {
-    if (!document.querySelector(".result").value) {return};
-    document.querySelector(".result").value /= 100;
-})
-
+numRootButton.addEventListener('click', performNumRoot);
+numByPowerButton.addEventListener('click', performNumByPower);
+percentButton.addEventListener('click', performPercentOperation);
 squareButton.addEventListener('click', () => getNumberInPower(2));
-
 cubeButton.addEventListener('click', () => getNumberInPower(3));
-
-oneDivByNumberButton.addEventListener('click', () => getOneDevidedByNumber());
-
-expButton.addEventListener('click', () => {
-    document.querySelector(".result").value = Math.exp(1);
-});
-
-clearButton.addEventListener('click', () => {
-    document.querySelector('.result').value = '';
-    document.querySelector('.archiv').value = '';
-    document.querySelector('.result').placeholder = '0';
-});
-
-backspaceButton.addEventListener('click', () => {
-    let numberWithoutLastSymbol = document.querySelector(".result").value.slice(0, -1);
-    document.querySelector(".result").value = numberWithoutLastSymbol;
-});
-
-addButton.addEventListener('click', (e) => {
-    setSimpleMathOptions(e.target.value);
-});
-
-subButton.addEventListener('click', (e) => {
-    setSimpleMathOptions(e.target.value);
-});
-
-mulButton.addEventListener('click', (e) => {
-    setSimpleMathOptions(e.currentTarget.value);
-});
-
-divButton.addEventListener('click', (e) => {
-    setSimpleMathOptions(e.currentTarget.value);
-});
-
-equalButton.addEventListener('click', () => {
-    let res;
-    if (!document.querySelector('.archiv').value) {return};
-    let sign = document.querySelector('.archiv').value.slice(-1);
-    let firstNumber = Number(document.querySelector('.archiv').value.slice(0, -1));
-    let secondNumber = Number(document.querySelector('.result').value);
-
-    switch (sign) {
-        case '+':
-            res = firstNumber + secondNumber;
-          break;
-        case '-':
-            res = firstNumber - secondNumber;
-          break;
-        case '*':
-            res = firstNumber * secondNumber;
-          break;
-        case '/':
-            if (secondNumber === 0) {
-                document.querySelector('.result').value = 'division by zero';
-                return
-            } else {
-                res = firstNumber / secondNumber;
-            }
-          break;
-        case '^':
-                res = Math.pow(firstNumber, secondNumber);
-          break;
-        case '√':
-                res = Math.pow(firstNumber, 1 / secondNumber);
-          break;
-        default:
-            return;
-    }
-   
-    document.querySelector('.archiv').value += secondNumber;
-    document.querySelector('.result').value = res;
-});
+oneDivByNumberButton.addEventListener('click', getOneDevidedByNumber);
+expButton.addEventListener('click', () => {document.querySelector(".result").value = Math.exp(1)});
+clearButton.addEventListener('click', performClearOperation);
+backspaceButton.addEventListener('click', performBackspaceOperation);
+addButton.addEventListener('click', (e) => {setSimpleMathOptions(e.target.value)});
+subButton.addEventListener('click', (e) => {setSimpleMathOptions(e.target.value)});
+mulButton.addEventListener('click', (e) => {setSimpleMathOptions(e.currentTarget.value)});
+divButton.addEventListener('click', (e) => {setSimpleMathOptions(e.currentTarget.value)});
+equalButton.addEventListener('click', performEqualOperation);
