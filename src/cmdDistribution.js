9d08ec2a40c7fcc6768110454ad36ced750684ae
Maksim Ashmiana <max.basic.94@gmai.com>
2021-11-08 12:29:22 +0300

    changed cmdDistribution.js

--- src/cmdDistribution.js
+++ src/cmdDistribution.js
@@ -6,30 +6,15 @@ import PowCommand from './commands/PowCommand'
 import SquareCommand from './commands/SquareCommand'
 
 function switchOperation(sign, firstNumber, secondNumber) {
-    let res;
     switch (sign) {
-      case '+':
-        res = new AddCommand(firstNumber, secondNumber).execute();
-        break;
-      case '-':
-        res = new SubCommand(firstNumber, secondNumber).execute();
-        break;
-      case '*':
-        res = new MulCommand(firstNumber, secondNumber).execute();
-        break;
-      case '/':
-        res = new DivCommand(firstNumber, secondNumber).execute();
-        break;
-      case '^':
-        res = new PowCommand(firstNumber, secondNumber).execute();
-        break;
-      case '√':
-        res = new SquareCommand(firstNumber, secondNumber).execute();
-        break;
-      default:
-        return;
+      case '+': return new AddCommand(firstNumber, secondNumber).execute();
+      case '-': return new SubCommand(firstNumber, secondNumber).execute();
+      case '*': return new MulCommand(firstNumber, secondNumber).execute();
+      case '/': return new DivCommand(firstNumber, secondNumber).execute();
+      case '^': return new PowCommand(firstNumber, secondNumber).execute();
+      case '√': return new SquareCommand(firstNumber, secondNumber).execute();
+      default: return;
     }
-    return res;
 }
 
 export default switchOperation;
