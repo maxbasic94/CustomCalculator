59b791848d163c4bed4608a3be7e019d073b6cd0
Maksim Ashmiana <max.basic.94@gmai.com>
2021-11-09 11:30:19 +0300

    refactored code

--- src/app.js
+++ src/app.js
@@ -7,35 +7,15 @@ const archivInput = document.querySelector('.archiv');
 const calc = new Calc(resultInput, archivInput);
 let memory = new Memory('');
 
-// document.querySelectorAll('.btn-numb').forEach((numberButton) => numberButton.addEventListener('click', (e) => {
-//     let number = e.target.value;
-//     let result = resultInput;
-//     result.value += number;
-//   })
-// )
-
-// document.querySelectorAll('.memory').forEach((memoryButton) => memoryButton.addEventListener('click', () => performMemoryOperation(memoryButton)));
-
-// document.querySelectorAll('.simpleOperation').forEach((simpleOperationButton) => simpleOperationButton.addEventListener('click', () => {
-//   if (archivInput.value) {
-//     let archivString = String(archivInput.value).match(/[\d\.\,]+/g);
-//     if (archivString.length > 1) {
-//       archivInput.value = resultInput.value + '+';
-//       resultInput.value = '';
-//     } else {
-//       let firstNumber = Number(archivString[0]);
-//       let secondNumber = Number(resultInput.value);
-//       let sign = archivInput.value.slice(-1);
-//       archivInput.value = new (switchOperation(sign))(firstNumber, secondNumber).execute() + simpleOperationButton.value;
-//       resultInput.value = '';
-//     }
-//   } else {
-//     let actualNumber = resultInput.value;
-//     archivInput.value = actualNumber + simpleOperationButton.value;
-//     resultInput.value = '';
-//     resultInput.placeholder = '';
-//   }
-// }));
+document.querySelector('.buttons').addEventListener('click', (e) => {
+  if (resultInput.value === 'division by zero') {
+    for (let button of document.querySelectorAll('.btn')) {
+      if (button.id !== 'clearButton') { button.setAttribute("disabled", true) }
+    }
+  } else {
+    return;
+  }
+});
 
 document.querySelectorAll('.btn').forEach((btn) => btn.addEventListener('click', (e) => {
   switch (e.currentTarget.id) {
@@ -154,62 +134,3 @@ document.querySelectorAll('.btn').forEach((btn) => btn.addEventListener('click',
       break;
   }
 }));
-
-document.querySelector('.buttons').addEventListener('click', (e) => {
-  if (resultInput.value === 'division by zero') {
-    for (let button of document.querySelectorAll('.btn')) {
-      if (button.id !== 'clearButton') { button.setAttribute("disabled", true) }
-    }
-  } else {
-    return;
-  }
-});
-
-// document.querySelector('#numRootButton').addEventListener('click', () => {
-//   if (!resultInput.value) { return };
-//   let actualNumber = resultInput.value;
-//   archivInput.value = actualNumber + '\u221A';
-//   resultInput.placeholder = '';
-//   resultInput.value = '';
-// });
-
-// document.querySelector('#numByPowerButton').addEventListener('click', () => {
-//   if (!resultInput.value) { return };
-//   let actualNumber = resultInput.value;
-//   archivInput.value = actualNumber + '^';
-//   resultInput.value = '';
-//   resultInput.placeholder = '';
-// });
-
-// document.querySelector('#clearButton').addEventListener('click', () => {
-//   resultInput.value = '';
-//   archivInput.value = '';
-//   resultInput.placeholder = '0';
-//   for (let button of document.querySelectorAll('.btn')) {
-//     button.removeAttribute("disabled");
-//   }
-// });
-
-// document.querySelector('#backspaceButton').addEventListener('click', () => {
-//   let numberWithoutLastSymbol = resultInput.value.slice(0, -1);
-//   if (numberWithoutLastSymbol === '') {
-//     resultInput.value = 0;
-//   } else {
-//     resultInput.value = numberWithoutLastSymbol;
-//   }
-// });
-
-// document.querySelector('#equalButton').addEventListener('click', () => {
-//   if (!archivInput.value) { return };
-//   let archivString = String(archivInput.value).match(/[\d\.\,]+/g)
-//   if (archivString.length === 1) {
-//     let sign = archivInput.value.slice(-1);
-//     let firstNumber = Number(archivInput.value.slice(0, -1));
-//     let secondNumber = Number(resultInput.value);
-//     let res = new (switchOperation(sign))(firstNumber, secondNumber).execute();
-//     if (!(sign === '^' && (secondNumber === 2 || secondNumber === 3))) { archivInput.value += secondNumber }
-//     resultInput.value = res;
-//   } else {
-//     return;
-//   }
-// });
